e01a2db4b2eecacb831ecca69de452df45bf3892
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2025-01-15 22:35:48 -0300

    feture: configuração do módulo de usuário

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { Mongoose } from 'mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'User',
+        schema: 'UsersSchema'
+      }
+    ]),
+    AuthModule
+  ],
   controllers: [UsersController],
   providers: [UsersService]
 })
