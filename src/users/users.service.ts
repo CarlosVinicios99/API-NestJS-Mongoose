d38663fc49e371c40ce1d52f6e419a7727638735
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2025-01-16 21:45:05 -0300

    feature: criação do serviço de cadastrar usuário

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt'
+import { Model } from 'mongoose';
+import { User } from './models/user.model';
+import { AuthService } from 'src/auth/auth.service';
+import { SignupDto } from './dto/signup.dto';
+
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+  constructor(
+    private readonly usersModel: Model<User>,
+    private readonly authService: AuthService
+  ){}
+
+  public async signup(signupDto: SignupDto): Promise<User>{
+    const user = new this.usersModel(signupDto)
+    return await user.save()
+  }
+
+
+}
