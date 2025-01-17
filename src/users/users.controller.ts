e7481e5566090fc43742964942de113b4d6e23dc
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2025-01-16 22:40:00 -0300

    feature: criação de serviço para buscar todos os usuários

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { SignupDto } from './dto/signup.dto';
 import { User } from './models/user.model';
 import { SigninDto } from './dto/signin.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
 export class UsersController {
@@ -21,6 +22,13 @@ export class UsersController {
   @HttpCode(HttpStatus.OK)
   public async signin(@Body() signinDto: SigninDto): Promise<{name: string, jwtToken: string, email: string}>{
     return this.usersService.signin(signinDto)
+  }
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(AuthGuard('jwt'))
+  public async findAll(): Promise<User[]>{
+    return this.usersService.findAll()
   }
 
 }
