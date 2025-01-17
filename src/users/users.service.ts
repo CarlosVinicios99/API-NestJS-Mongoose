290055b01bfdab1ef593f711a464f35469097268
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2025-01-16 21:53:59 -0300

    feature: criação de serviços auxiliares para buscar usuario por email e para checar a criptografia da senha

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import * as bcrypt from 'bcrypt'
 import { Model } from 'mongoose';
 import { User } from './models/user.model';
 import { AuthService } from 'src/auth/auth.service';
 import { SignupDto } from './dto/signup.dto';
+import { SigninDto } from './dto/signin.dto';
 
 
 @Injectable()
@@ -14,10 +15,36 @@ export class UsersService {
     private readonly authService: AuthService
   ){}
 
+
   public async signup(signupDto: SignupDto): Promise<User>{
     const user = new this.usersModel(signupDto)
     return await user.save()
   }
 
+  public async signin(signinDto: SigninDto): Promise<{name: string, jwtToken: string, email: string}>{
+    const user = await this.findByEmail(signinDto.email)
+  }
+
+  private async findByEmail(email: string): Promise<User>{
+    const user: User = await this.usersModel.findOne({
+      email: email
+    })
+
+    if(!user){
+      throw new NotFoundException(`Email not found`)
+    }
+
+    return user
+  }
+
+  private async checkPassword(password: string, user: User): Promise<Boolean> {
+    const match: boolean = bcrypt.compare(password, user.password)
+
+    if(!match){
+      throw new NotFoundException(`Password not found`)
+    }
+
+    return match
+  } 
 
 }
